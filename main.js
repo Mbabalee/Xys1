a57ff6f8b568db9adab176177f1a7ea2fe2e3409
Mbabaali Henry <132438603+Mbabalee@users.noreply.github.com>
2024-11-21 18:35:11 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ menubtn.addEventListener("click",function(){
 var dropdown = document.getElementById("dropdwn")
 dropdown.classList.toggle("show")
 menubtn.style.display = "none"
-ex.style.opacity = 100
 ex.style.display = "block"
 
 
@@ -15,34 +14,8 @@ ex.addEventListener("click",function(){
     var dropdown = document.getElementById("dropdwn")
     dropdown.classList.toggle("show")
     menubtn.style.display = "block"
-    ex.style.opacity = 0
     ex.style.display = "none"
 
     })
-
-var log = document.getElementById("log")
-var reg = document.getElementById("reg")
-
-var form = document.getElementById("forms")
-var lg = document.getElementById("lgbx")
-var rep = document.getElementById("regbx")
-
-
-
-var ind = document.getElementById("ind")
-
-
-log.addEventListener("click",function(){
- rep.style.display = "none"
- lg.style.display = "flex"
- ind.style.translate = "50px"
-})
-reg.addEventListener('click',function(){
- lg.style.display = "none"
- rep.style.display = "flex"
- ind.style.translate = "-36px"
-
-});
-
 
 
